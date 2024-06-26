69ad9f34f5471e78a28a59c6864d8b6281fe0aaf
Smiley98 <smileysofficialcinema@gmail.com>
2024-06-26 12:47:20 -0400

    Add and remove

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-void PrintNumbers(int numbers[])
-{
-	for (int i = 0; i < 3; i++)
-		std::cout << numbers[i] << std::endl;
-}
-
-void PrintNumbers(int* numbers)
-{
-	for (int i = 0; i < 3; i++)
-		std::cout << numbers[i] << std::endl;
-}
+#include <vector>
 
 int main()
 {
-	int numbers[]{ 1, 2, 3 };
-	PrintNumbers(numbers);
+	std::vector<int> numbers;
+	for (int i = 0; i < 3000000; i++)
+		numbers.push_back(i);
+	
+	for (int i = 0; i < 3000000; i++)
+		numbers.pop_back();
+
+	// Much better!
 	return 0;
 }
+
+//numbers.clear();
+//std::cout << numbers.size() << std::endl;		// 0
+//std::cout << numbers.capacity() << std::endl;	// 3,543,306
+// Pro tip: if you delete all your code, you won't have any bugs!
