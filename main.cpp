f52a3ce55f914f3eef32016282a577bcb2e63942
Smiley98 <smileysofficialcinema@gmail.com>
2024-06-25 22:57:35 -0400

    w2

--- main.cpp
+++ main.cpp
@@ -1,48 +1,19 @@
 #include <iostream>
-
-class Player1
+void PrintNumbers(int numbers[])
 {
-public:
-	int health;
-
-	void SetHealth(int heath)
-	{
-		this->health = heath;
-	}
-};
-
-struct Player2
-{
-public:
-	int health;
-};
-
-void SetHealth(Player2 player, int health)
-{
-	player.health = health;
+	for (int i = 0; i < 3; i++)
+		std::cout << numbers[i] << std::endl;
 }
 
-bool CheckHealth(int health, int requiredHealth)
+void PrintNumbers(int* numbers)
 {
-	return health > requiredHealth;
+	for (int i = 0; i < 3; i++)
+		std::cout << numbers[i] << std::endl;
 }
 
 int main()
 {
-	int warriorHealths[5];
-	warriorHealths[0] = 100;
-	warriorHealths[1] = 69;
-	warriorHealths[2] = 420;
-	warriorHealths[3] = 42;
-	warriorHealths[4] = -9000;
-
-	int requiredHealth = 50;
-	for (int i = 0; i < 5; i++)
-	{
-		if (CheckHealth(warriorHealths[i], requiredHealth))
-			std::cout << "Warrior " << i << " ready for battle!" << std::endl;
-		else
-			std::cout << "Warrior " << i << " must rest!" << std::endl;
-	}
+	int numbers[]{ 1, 2, 3 };
+	PrintNumbers(numbers);
 	return 0;
 }
