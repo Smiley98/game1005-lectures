3521598109d4925671f0387137e9711397a32d5d
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-12 19:32:15 -0400

    Added value vs address

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-//#include <iostream>
+#include <iostream>
 int main()
 {
-	int numbers[]{ 1, 2, 3 };
+	int x = 1;
+	int* xAddr = &x;
+	std::cout << "Value of x is: " << x << std::endl;
+	std::cout << "Address of x is: " << xAddr << std::endl;
 	return 0;
 }
