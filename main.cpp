9f977e9205a438f71c1d30818db4ef5bbfff0591
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-12 18:47:01 -0400

    Switched to memory

--- main.cpp
+++ main.cpp
@@ -1,36 +1,6 @@
-#include <iostream>
-#include <array>
-#include <string>
-
-struct Person
-{
-	std::string name;
-	int age;
-};
-
-void CreatePeople(std::array<Person, 3>& people)
-{
-	for (int i = 0; i < people.size(); i++)
-	{
-		// TODO -- assign each person a name and age
-		people[i].age = 26;
-		people[i].name = "Connor";
-	}
-}
-
-void PrintPeople(std::array<Person, 3> people)
-{
-	for (int i = 0; i < people.size(); i++)
-	{
-		std::cout << people[i].name << std::endl;
-		std::cout << people[i].age << std::endl;
-	}
-}
-
+//#include <iostream>
 int main()
 {
-	std::array<Person, 3> people;
-	CreatePeople(people);
-	PrintPeople(people);
+	int numbers[]{ 1, 2, 3 };
 	return 0;
 }
