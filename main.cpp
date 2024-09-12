a4f3935e65e2521c16501f78655fe2802403cbfc
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-12 19:39:37 -0400

    Added dereference

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 	int x = 1;
 	int* xAddr = &x;
 	std::cout << "Value of x is: " << x << std::endl;
-	std::cout << "Address of x is: " << xAddr << std::endl;
+	*xAddr = x + 1;
+	std::cout << "Value of x is: " << *xAddr << std::endl;
 	return 0;
 }
