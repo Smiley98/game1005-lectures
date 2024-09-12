102ac3dda46c2afc424986302132e898a5295bac
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-12 19:41:58 -0400

    Added task 1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ int main()
 	int x = 1;
 	int* xAddr = &x;
 	std::cout << "Value of x is: " << x << std::endl;
-	*xAddr = x + 1;
-	std::cout << "Value of x is: " << *xAddr << std::endl;
+	std::cout << "Address of x is: " << xAddr << std::endl;
+
+	// TODO -- do the same thing, but for y and yAddr, and z and zAddr!
+
 	return 0;
 }
