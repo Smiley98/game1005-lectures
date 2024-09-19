7f27d2ae44434c2f4273070034180b415c016e20
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-19 17:38:56 -0400

    Added w3 start

--- main.cpp
+++ main.cpp
@@ -1,36 +1,7 @@
 #include <iostream>
-#include <array>
-#include <string>
-
-struct Person
-{
-	std::string name;
-	int age;
-};
-
-void CreatePeople(std::array<Person, 3> people)
-{
-	for (int i = 0; i < people.size(); i++)
-	{
-		// TODO -- assign each person a name and age
-	}
-}
-
-void PrintPeople(std::array<Person, 3> people)
-{
-	for (int i = 0; i < people.size(); i++)
-	{
-		std::cout << people[i].name << std::endl;
-		std::cout << people[i].age << std::endl;
-	}
-}
-
-// TODO -- create this function with a native array. Does it produce the same result?
 
 int main()
 {
-	std::array<Person, 3> people;
-	CreatePeople(people);
-	PrintPeople(people);
+
 	return 0;
 }
