af76fd00852801cac72446ce2ca185373155847d
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-19 18:06:52 -0400

    vector push_back and pop_back

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	std::vector<int> numbers;
+
+	for (int i = 0; i < 3000000; i++)
+		numbers.push_back(i);
+
+	int front = numbers.front();
+	int back = numbers.back();
+
+	for (int i = 0; i < 3000000; i++)
+		numbers.pop_back();
 
 	return 0;
 }
