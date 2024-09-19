82fee924510393ecfaf92f35cd0abd4c0a591276
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-19 19:06:34 -0400

    Added homework1 start

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <limits>
 
-void HurtMyComputer()
+void PushBack(int*& numbers, int number, int& size, int& capacity)
 {
-	int* numbers = new int[INT_MAX];
+	if (size + 1 >= capacity)
+	{
+		// Perform reallocation
+	}
+	// Add number to the back of numbers
 }
 
-int* PotentiallyHurtMyComputer()
+void PopBack(int& size)
 {
-	int* numbers = new int[INT_MAX];
-	return numbers;
+	// Implementation is up to you. Modify function parameters as you see fit!
+	// I recommend you simply decrement size (since size is independent of capacity).
 }
 
 int main()
 {
-	// "Leak" 8 gigabytes
-	HurtMyComputer();
+	int size = 0;
+	int capacity = 3;
+	int* numbers = new int[capacity];
+	for (int i = 0; i < capacity; i++)
+	{
+		numbers[i] = i + 1;
+		size++;
+	}
 
-	// Store the address of our 8 gigabyte allocation so we can (ideally) delete it later!
-	int* memory = PotentiallyHurtMyComputer();
-	delete[] memory;
+	// Should print "1, 2, 3, 420"
+	std::cout << "PushBack called. Desired Output: 1, 2, 3, 420" << std::endl;
+	PushBack(numbers, 420, size, capacity);
+	for (int i = 0; i < size; i++)
+		std::cout << numbers[i] << std::endl;
+
+	// Should print "1, 2, 3"
+	std::cout << std::endl << "PopBack called. Desired Output: 1, 2, 3" << std::endl;
+	PopBack(size);
+	for (int i = 0; i < size; i++)
+		std::cout << numbers[i] << std::endl;
+
+	// Tip: use the debugger to inspect local variables and memory!
+	// Tip: if you don't have any code, you don't have any bugs. ;)
 	return 0;
 }
